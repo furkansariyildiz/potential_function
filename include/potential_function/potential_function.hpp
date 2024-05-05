707e35fef591d69ce917d1359c5a9ffe89336db3
furkansariyildiz <furkansariyildiz@windowslive.com>
2024-05-05 18:36:44 +0300

    cpp and hpp files are generated.

---
+++ include/potential_function/potential_function.hpp
@@ -0,0 +1,21 @@
+#ifndef POTENTIAL_FUNCTION_HPP
+#define POTENTIAL_FUNCTION_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/time.hpp>
+#include <iostream>
+
+using namespace std;
+
+
+
+class PotentialFunction
+{
+    private:
+
+    public:
+        PotentialFunction();
+        ~PotentialFunction();
+};
+
+#endif
---
+++ src/potential_function.cpp
@@ -0,0 +1,25 @@
+#include <potential_function/potential_function.hpp>
+
+using namespace std;
+
+
+
+PotentialFunction::PotentialFunction()
+{
+
+}
+
+
+
+PotentialFunction::~PotentialFunction()
+{
+
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
